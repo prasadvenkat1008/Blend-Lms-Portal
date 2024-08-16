312286fedad3035832ca39f7be560d671b8b59ff
prasad456789 <iiittstudent755@gmail.com>
2024-08-16 19:16:40 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -138,9 +138,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR,'static')
+#]
 
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
