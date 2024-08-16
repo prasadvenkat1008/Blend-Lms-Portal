593a2ca08599c9526b63f2cf785990f67695aa8d
prasad456789 <iiittstudent755@gmail.com>
2024-08-16 16:44:52 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'staticfiles')
 ]
 
 MEDIA_URLS ='/media/'
