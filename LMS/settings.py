7571c7a2e140575c78af2554beffa0661bd15efb
prasad456789 <iiittstudent755@gmail.com>
2024-08-16 17:11:34 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR,'staticfiles')
