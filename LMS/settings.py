f4a271db76d72508c4ed8a3d7c12f0fac5a16da8
prasad456789 <iiittstudent755@gmail.com>
2024-08-16 16:20:39 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
