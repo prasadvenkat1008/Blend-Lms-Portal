21cd98380741514407b0686f1dc906c402b5ae17
prasad456789 <iiittstudent755@gmail.com>
2024-08-16 15:46:35 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-0ks-8)i(9#dzlvx9)ze^%t#ulgh0h2s#e&jay0nh6^y!qf5ok%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['13.233.111.154','localhost']
+
 
 
 
