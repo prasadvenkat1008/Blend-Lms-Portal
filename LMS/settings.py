0d299866c23bb6e41474585d52e433bbdd874c51
prasad456789 <iiittstudent755@gmail.com>
2024-08-18 23:07:47 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0ks-8)i(9#dzlvx9)ze^%t#ulgh0h2s#e&jay0nh6^y!qf5ok%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['13.233.111.154','localhost']
+ALLOWED_HOSTS = ['13.233.111.154','localhost','internvidya.online']
 
 
 
@@ -142,8 +142,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 #    os.path.join(BASE_DIR,'static')
 #]
 
-MEDIA_URLS ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URLS ='/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
